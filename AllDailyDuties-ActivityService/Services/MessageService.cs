a1c25bba2eeca49d31c59a80e8e02eda19d6d384
Koen-sanders <33812471+Koen-sanders@users.noreply.github.com>
2023-01-17 18:02:44 +0100

    Update MessageService.cs

--- AllDailyDuties-ActivityService/Services/MessageService.cs
+++ AllDailyDuties-ActivityService/Services/MessageService.cs
@@ -69,7 +69,7 @@ namespace AllDailyDuties_ActivityService.Services
                 dynamic items = response.Resource;
 
                 // Update the 'Status' property
-                items.Status = "Assigned!";
+                items.Status = "AssignedNew";
 
                 // Replace the document
                 response = await container.ReplaceItemAsync<dynamic>(items, mainId, new PartitionKey(mainId.ToString()));
